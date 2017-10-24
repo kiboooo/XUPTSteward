3333ee24dd1f8736c962d4ee2117fdae56324ea3
kiboooo <kiboooo78@gmail.com>
2017-10-25 01:12:03 +0800

    增加了启动页

--- app/src/main/java/com/example/dickjampink/logintest/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/dickjampink/logintest/activity/WelcomeActivity.java
@@ -23,6 +23,7 @@ public class WelcomeActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
